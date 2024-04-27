d4298173c5c14ee75f4a0f843f2d5dab083b69c5
Argo Center <chevldmr@gmail.com>
2024-04-27 16:00:06 -0400

    functions and vars

--- js/example.js
+++ js/example.js
@@ -1,1 +1,14 @@
-alert(987);
+let a = 2;
+let b = 2;
+console.log(a + b);
+
+function sum(a, b) {
+    return a + b;
+}
+
+this.anotherSum = function(a, b) {
+    return a + b;
+}
+
+console.log(anotherSum(a, b));
+console.log(sum("2","2"));
