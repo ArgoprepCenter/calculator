f11ab76510becd3c02bd1c7290d3bdec49f32ed9
Argo Center <chevldmr@gmail.com>
2024-05-25 16:00:45 -0400

    test

--- js/example.js
+++ js/example.js
@@ -1,15 +1,22 @@
 let a = 2;
 let b = 2;
-console.log(a + b);
+// 4+2(3*7-5)/2=?
+// 4+2-1
+console.log(substraction(sum(4, 2), 1));
+
 
 function sum(a, b) {
     return a + b;
 }
 
-this.anotherSum = function(a, b) {
-    return a + b;
+function division(a, b) {
+    return a / b;
 }
 
-console.log(anotherSum(a, b));
-console.log(sum("2", "2"));
-console.log(sum(a, b));
+function multiplication(a, b) {
+    return a * b;
+}
+
+function substraction(a, b) {
+    return a - b;
+}
